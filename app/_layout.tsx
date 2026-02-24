c550006ed8288eba4fcad46436c4129b02c7bb83
josecaselli <josejuancaselli@gmail.com>
2026-02-24 20:28:00 -0300

    cosas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { WorkoutProvider } from "../context/WorkoutContext";
 
@@ -6,6 +7,7 @@ export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <WorkoutProvider>
+        <StatusBar style="dark" />
         <Stack />
       </WorkoutProvider>
     </GestureHandlerRootView>
